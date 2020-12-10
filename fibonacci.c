23d74506fe1bb5e26ba364b169945f87f8b879dd
Aman Maurya <amanmaurya2002@gmail.com>
2020-12-10 15:42:53 +0530

    Initial commit

---
+++ fibonacci.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+
+void fibonacci(int i1, int i2, int n);
+
+int main(void){
+printf("Hello, World!\n");
+int n;
+do{
+	printf("Enter the number( > 2) of numbers in the fibonacci sequence you want: ");
+	scanf("%i", &n);
+}while(n < 3);
+printf("0 1 ");
+fibonacci(0, 1, n - 2);
+printf("\n");
+}
+
+//a recursive function for fibonacci sequence
+void fibonacci(int i1, int i2, int n){
+int i = i1 + i2;
+i1 = i2;
+i2 = i;
+printf("%i ", i);
+if(n > 1){
+	fibonacci(i1, i2, n-1);
+	}
+}
---
+++ sum.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+
+int main(void){
+    printf("Hello, World!\n");
+    int a, b;
+    printf("Enter first number: ");
+    scanf("%i",&a);
+    printf("Enter second number: ");
+    scanf("%i",&b);
+    printf("%i\n",a+b);
+    return 0;
+}
