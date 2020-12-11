a9d693cac7b090853b107937e1f66ecb2b1d381a
Aman Maurya <amanmaurya2002@gmail.com>
2020-12-11 14:14:30 +0530

    fixed some bugs

--- sum.c
+++ sum.c
@@ -3,7 +3,7 @@
 int main(void){
     printf("\nHello, World!\n\n");
     int a, b;
-    printf("This is a program to add to integers.\n\n");
+    printf("This is a program to add two integers.\n\n");
     printf("Enter first number: ");
     scanf("%i",&a);
     printf("Enter second number: ");
